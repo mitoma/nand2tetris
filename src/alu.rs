052efe9df60b4dd230668a05574e40f04b0366fa
mitoma <mutetheradio@gmail.com>
2018-02-26 23:31:41 +0900

    ALUがとりあえず実装できた気がする

--- src/alu.rs
+++ src/alu.rs
@@ -1,5 +1,7 @@
 use const_value;
 use adder::*;
+use basic_gate::*;
+use multi_gate::*;
 
 pub fn alu(
     x: [bool; 16],
@@ -11,6 +13,41 @@ pub fn alu(
     f: bool,
     no: bool,
 ) -> ([bool; 16], bool, bool) {
-    add16(const_value::ZERO, y);
-    ([false; 16], false, false)
+    let x = mux4way16(
+        x,
+        const_value::ZERO,
+        not16(x),
+        /* undefined value*/ const_value::FULL,
+        [zx, nx],
+    );
+    let y = mux4way16(
+        y,
+        const_value::ZERO,
+        not16(y),
+        /* undefined value*/ const_value::FULL,
+        [zy, ny],
+    );
+
+    let result = mux4way16(
+        and16(x, y),
+        add16(x, y),
+        not16(and16(x, y)),
+        not16(and16(x, y)),
+        [f, no],
+    );
+
+    return (
+        result,
+        and(result[0], true),
+        nand(
+            nand(
+                nand(nand(result[0], result[1]), nand(result[2], result[3])),
+                nand(nand(result[4], result[5]), nand(result[6], result[7])),
+            ),
+            nand(
+                nand(nand(result[8], result[9]), nand(result[10], result[11])),
+                nand(nand(result[12], result[13]), nand(result[14], result[15])),
+            ),
+        ),
+    );
 }
