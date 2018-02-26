489549ad3bbc9753f4a2a7e92efb5df7fdd17253
mitoma <mutetheradio@gmail.com>
2018-02-26 22:49:45 +0900

    ALUのIFだけ追加

---
+++ src/alu.rs
@@ -0,0 +1,12 @@
+pub fn alu(
+    x: [bool; 16],
+    y: [bool; 16],
+    zx: bool,
+    nx: bool,
+    zy: bool,
+    ny: bool,
+    f: bool,
+    no: bool,
+) -> ([bool; 16], bool, bool) {
+    ([false; 16], false, false)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod basic_gate;
 pub mod multi_gate;
 pub mod adder;
+pub mod alu;
 pub mod test_util;
