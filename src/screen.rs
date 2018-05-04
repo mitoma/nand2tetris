679ae8050afe4e999912eb521ed3a8143a4015c7
mitoma <mutetheradio@gmail.com>
2018-05-05 01:37:19 +0900

    screen の実装。多分できてるはず。

--- src/screen.rs
+++ src/screen.rs
@@ -23,16 +23,40 @@ impl Screen {
     }
 
     pub fn draw(&mut self, e: &Event) {
+        let ram = &self.ram;
         self.window.draw_2d(e, |c, g| {
             // clear window
             clear([0.0, 0.0, 0.0, 1.0], g);
-            //
-            rectangle(
-                [1.0, 0.0, 0.0, 1.0],     // color
-                [0.0, 0.0, 100.0, 100.0], // dot
-                c.transform,
-                g,
-            );
+
+            // draw memory
+            let mut counter: u32 = 0;
+            for rams4k in ram.rams[0..1].iter() {
+                for rams512 in rams4k.rams.iter() {
+                    for ram64 in rams512.rams.iter() {
+                        for ram8 in ram64.rams.iter() {
+                            for register in ram8.registers.iter() {
+                                for bit in register.bits.iter() {
+                                    let color = if bit.dff.pre_value {
+                                        [1.0, 1.0, 1.0, 1.0]
+                                    } else {
+                                        [0.0, 0.0, 0.0, 1.0]
+                                    };
+                                    let x = counter % 512;
+                                    let y = counter / 512;
+                                    rectangle(
+                                        color,                                                    // color
+                                        [x as f64, y as f64, (x as f64 + 1.0), (y as f64 + 1.0)], // dot
+                                        c.transform,
+                                        g,
+                                    );
+                                    counter += 1;
+                                }
+                            }
+                        }
+                    }
+                }
+                println!("{}", counter);
+            }
         });
     }
 
