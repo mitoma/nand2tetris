97481d20e0afb249b13e0fda0f9bd8162f102955
mitoma <mutetheradio@gmail.com>
2018-05-10 23:56:40 +0900

    シフトキーの実装の検討

--- src/screen.rs
+++ src/screen.rs
@@ -8,6 +8,7 @@ use test_util::*;
 pub struct Screen {
     pub ram: Ram16k,
     pub window: PistonWindow,
+    on_shift: bool,
 }
 
 impl Screen {
@@ -21,64 +22,81 @@ impl Screen {
         Screen {
             ram: ram,
             window: window,
+            on_shift: false,
         }
     }
 
     pub fn draw(&mut self, e: &Event) {
-        let ram = &mut self.ram;
-
-        let mut canvas = ImageBuffer::new(512, 256);
-        // draw memory
-        let mut counter: u32 = 0;
-        for rams4k in ram.rams[0..2].iter() {
-            for rams512 in rams4k.rams.iter() {
-                for ram64 in rams512.rams.iter() {
-                    for ram8 in ram64.rams.iter() {
-                        for register in ram8.registers.iter() {
-                            for bit in register.bits.iter() {
-                                let color = if bit.dff.pre_value {
-                                    [0, 255, 0, 255]
-                                } else {
-                                    [0, 0, 0, 255]
-                                };
-                                let x = counter % 512;
-                                let y = counter / 512;
-                                canvas.put_pixel(x, y, Rgba(color));
-                                counter += 1;
+        if let Some(_) = e.update_args() {
+            let ram = &mut self.ram;
+            let mut canvas = ImageBuffer::new(512, 256);
+            // draw memory
+            let mut counter: u32 = 0;
+            for rams4k in ram.rams[0..2].iter() {
+                for rams512 in rams4k.rams.iter() {
+                    for ram64 in rams512.rams.iter() {
+                        for ram8 in ram64.rams.iter() {
+                            for register in ram8.registers.iter() {
+                                for bit in register.bits.iter() {
+                                    let color = if bit.dff.pre_value {
+                                        [0, 255, 0, 255]
+                                    } else {
+                                        [0, 0, 0, 255]
+                                    };
+                                    let x = counter % 512;
+                                    let y = counter / 512;
+                                    canvas.put_pixel(x, y, Rgba(color));
+                                    counter += 1;
+                                }
                             }
                         }
                     }
                 }
             }
+            let texture: G2dTexture =
+                Texture::from_image(&mut self.window.factory, &canvas, &TextureSettings::new())
+                    .unwrap();
+            self.window.draw_2d(e, |c, g| {
+                image(&texture, c.transform, g);
+            });
         }
-        let texture: G2dTexture =
-            Texture::from_image(&mut self.window.factory, &canvas, &TextureSettings::new())
-                .unwrap();
-        self.window.draw_2d(e, |c, g| {
-            image(&texture, c.transform, g);
-        });
     }
 
     pub fn key(&mut self, e: &Event) {
         // http://docs.piston.rs/mush/piston/input/keyboard/enum.Key.html
         if let Some(key) = e.press_args() {
+            if self.is_shift(key) {
+                self.on_shift = false;
+            }
             let key_bits = i2b(self.key_to_code(key));
-            println!("keycoard {}", self.key_to_code(key));
 
             let ram = &mut self.ram;
             ram.ram(
                 key_bits,
                 [
-                    false, false, false, false, false, true, false, false, false, false, false,
+                    false, false, false, false, false, true, false, false, false, false, true,
                     false, false, false,
                 ],
                 true,
             );
         }
+        if let Some(key) = e.release_args() {
+            if self.is_shift(key) {
+                self.on_shift = false;
+            }
+        }
+    }
+
+    fn is_shift(&mut self, key: Button) -> bool {
+        match key {
+            Button::Keyboard(keyboard::Key::LShift) | Button::Keyboard(keyboard::Key::RShift) => {
+                true
+            }
+            _ => false,
+        }
     }
 
     fn key_to_code(&mut self, key: Button) -> i16 {
-        println!("to : {:?}", key);
         match key {
             Button::Keyboard(keyboard::Key::Space) => 32,
 
