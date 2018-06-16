ebcd0c4addbc8592610a4c775fdd07903ecf23fa
mitoma <mutetheradio@gmail.com>
2018-06-16 15:41:42 +0900

    CPUの実装がまずは完了。

--- src/cpu.rs
+++ src/cpu.rs
@@ -37,10 +37,8 @@ impl Cpu {
         let a_register_current_value = self.a_register.register(ZERO, false);
         let d_register_current_value = self.d_register.register(ZERO, false);
 
-        let is_c = instruction[0]; // C命令なら true
-
-        let use_memory = instruction[3]; //
-
+        let is_c_command = instruction[0]; // C命令なら true
+        let use_memory = instruction[3];
         let alu_y_value = mux16(a_register_current_value, in_memory, use_memory);
 
         let alu_result = alu(
@@ -57,47 +55,66 @@ impl Cpu {
         let dest = [instruction[10], instruction[11], instruction[12]];
         let jump = [instruction[13], instruction[14], instruction[15]];
 
-        let a_register_store_value = mux16(alu_result.0, instruction, is_c);
+        let a_register_store_value = mux16(alu_result.0, instruction, is_c_command);
         self.a_register.register(
             a_register_store_value,
             or(
-                /* A命令の時 */ not(is_c),
+                /* A命令の時 */ not(is_c_command),
                 /* C命令の時のフラグ */ dest[0],
             ),
         );
         self.d_register.register(alu_result.0, dest[1]);
 
-        // A命令時のvalue。これはいらないかも
-        let value = [
-            instruction[1],
-            instruction[2],
-            instruction[3],
-            instruction[4],
-            instruction[5],
-            instruction[6],
-            instruction[7],
-            instruction[8],
-            instruction[9],
-            instruction[10],
-            instruction[11],
-            instruction[12],
-            instruction[13],
-            instruction[14],
-            instruction[15],
-        ];
+        let alu_out_is_zero = alu_result.1;
+        let alu_out_is_negative = alu_result.2;
+        let alu_out_is_positive = and(not(alu_out_is_zero), not(alu_out_is_negative));
 
-        // TODO ここが山だ
-        // pub fn count(&mut self, a: [bool; 16], inc: bool, load: bool, reset: bool) -> [bool; 16] {
-        // pc.count(a_register_current_value, /* TODO : load */ ,reset)
+        let load = or(
+            or(
+                and(jump[0], alu_out_is_negative),
+                and(jump[0], alu_out_is_zero),
+            ),
+            and(jump[2], alu_out_is_positive),
+        );
+        let pc_result = self.pc.count(a_register_current_value, true, load, reset);
 
-        let mut address_memory = [false; 15];
-        address_memory.copy_from_slice(&a_register_current_value[0..15]);
-        // 暫定の返り値
         CpuResult {
             out_memory: alu_result.0,
             write_memory: dest[2],
-            address_memory: address_memory,
-            pc: [false; 15],
+            address_memory: [
+                a_register_current_value[0],
+                a_register_current_value[1],
+                a_register_current_value[2],
+                a_register_current_value[3],
+                a_register_current_value[4],
+                a_register_current_value[5],
+                a_register_current_value[6],
+                a_register_current_value[7],
+                a_register_current_value[8],
+                a_register_current_value[9],
+                a_register_current_value[10],
+                a_register_current_value[11],
+                a_register_current_value[12],
+                a_register_current_value[13],
+                a_register_current_value[14],
+            ],
+            pc: [
+                pc_result[0],
+                pc_result[1],
+                pc_result[2],
+                pc_result[3],
+                pc_result[4],
+                pc_result[5],
+                pc_result[6],
+                pc_result[7],
+                pc_result[8],
+                pc_result[9],
+                pc_result[10],
+                pc_result[11],
+                pc_result[12],
+                pc_result[13],
+                pc_result[14],
+            ],
         }
     }
 }
