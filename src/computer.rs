55d7074afc31927cb834cd0674f1123befc3e48c
mitoma <mutetheradio@gmail.com>
2018-06-19 15:22:23 +0900

    Computerの実装

--- src/computer.rs
+++ src/computer.rs
@@ -1,4 +1,7 @@
+use basic_gate::*;
+use const_value::*;
 use cpu::{Cpu, CpuResult};
+use multi_gate::*;
 use ram::{Ram16kHiSpeed, Ram32kHiSpeed};
 use screen::Screen;
 
@@ -6,6 +9,8 @@ pub struct Computer {
     pub rom: Ram32kHiSpeed,
     pub ram: Ram16kHiSpeed,
     pub screen: Screen,
+    pub cpu: Cpu,
+    pub pre_cpu_result: CpuResult,
 }
 
 impl Computer {
@@ -14,8 +19,32 @@ impl Computer {
             rom: Ram32kHiSpeed::new(),
             ram: Ram16kHiSpeed::new(),
             screen: Screen::new(Ram16kHiSpeed::new()),
+            cpu: Cpu::new(),
+            pre_cpu_result: CpuResult {
+                out_memory: [false; 16],
+                write_memory: false,
+                address_memory: [false; 15],
+                pc: [false; 15],
+            },
         }
     }
 
-    pub fn cycle(&mut self, reset: bool) {}
+    pub fn cycle(&mut self, reset: bool) {
+        let add15 = self.pre_cpu_result.address_memory;
+        let add14 = [
+            add15[0], add15[1], add15[2], add15[3], add15[4], add15[5], add15[6], add15[7],
+            add15[8], add15[9], add15[10], add15[11], add15[12], add15[13],
+        ];
+
+        let ram_data = self.ram
+            .ram(self.pre_cpu_result.out_memory, add14, not(add15[14]));
+        let io_data = self.screen
+            .ram
+            .ram(self.pre_cpu_result.out_memory, add14, add15[14]);
+
+        let in_memory = mux16(ram_data, io_data, add15[14]);
+
+        let instruction = self.rom.ram(ZERO, self.pre_cpu_result.pc, false);
+        self.pre_cpu_result = self.cpu.cycle(in_memory, instruction, reset);
+    }
 }
