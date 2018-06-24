7d2fbd52ec47d9bb7404f3f80df7cc17858285c6
mitoma <mutetheradio@gmail.com>
2018-06-24 22:07:13 +0900

    引数の取り方を変えた＆デバッグしやすいように指定クロック数で中断できるようにした。

--- src/computer_runner.rs
+++ src/computer_runner.rs
@@ -11,18 +11,34 @@ fn main() {
     let thread = builder.stack_size(10000000);
     let handle = thread
         .spawn(|| {
-            let args: Vec<String> = env::args().collect();
-            let program_path = &args[1];
+            let program_path = env::args().nth(1);
+            let max_cycle_count = env::args()
+                .nth(2)
+                .ok_or("max_cycle_count is none.".to_owned())
+                .and_then(|v| {
+                    v.parse::<i32>()
+                        .map_err(|_err| "please input number".to_owned())
+                });
             let mut computer = Computer::new();
-            computer.load_rom(program_path.to_string());
+            match program_path {
+                Some(path) => computer.load_rom(path),
+                None => {
+                    println!(
+                        "{}",
+                        "hack バイナリのパスを指定してください"
+                    );
+                    std::process::exit(0)
+                }
+            }
             let mut counter = 0;
             while let Some(e) = computer.screen.window.next() {
                 computer.cycle(false);
                 computer.screen.draw(&e);
                 computer.screen.key(&e);
                 counter += 1;
-                if counter > 50 {
-//                    std::process::exit(0);
+                match max_cycle_count {
+                    Ok(value) if counter > value => std::process::exit(0),
+                    _ => {}
                 }
             }
         })
